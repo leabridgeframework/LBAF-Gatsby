af3c50cb042452b09027f75da18a6a3bd63d5120
ProjectCentre <ProjectCentre@users.noreply.github.com>
2022-03-15 18:24:41 +0000

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import {StaticImage} from 'gatsby-plugin-image'
 
 
 // Step 2: Define your component
@@ -8,6 +9,10 @@ const IndexPage = () => {
   return (
   	<Layout pageTitle = "Home Page">
   		<p> I'm making this using the Gatsby Tutorial thing and i'm rubbish</p>
+  		<StaticImage
+  			alt="a lovely landscape from unsplash"
+        src="../images/Landscape-unsplash.jpg"
+        />
   	</Layout>
   )
 }
